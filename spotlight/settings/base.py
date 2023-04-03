cfdf4c8a8d72a13203e92cd45a606ee6380afe2a
zahra.bahrami <z.bahrami@danalytica.com>
2023-04-04 00:36:20 +0430

    settings file updated

--- spotlight/settings/base.py
+++ spotlight/settings/base.py
@@ -156,7 +156,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
